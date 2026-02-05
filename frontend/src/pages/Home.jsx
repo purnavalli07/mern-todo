218c41fd9133bd19cdcd7c3fbcda3cb93325eb45
Purnavalli <purnavalliryali@gmail.com>
2026-02-05 22:04:00 +0530

    Task 5: Fetch and display todos from backend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,36 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 function Home() {
-  return <h2>Todo List Page</h2>;
+
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const fetchTodos = async () => {
+    try {
+      const res = await axios.get("http://localhost:5050/api/todos");
+      setTodos(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Todo List</h2>
+
+      {todos.map((todo) => (
+        <div key={todo._id}>
+          <h3>{todo.title}</h3>
+          <p>{todo.description}</p>
+        </div>
+      ))}
+
+    </div>
+  );
 }
 
 export default Home;
